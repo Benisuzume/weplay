391ab2be934ca5d683c7da85e315dbcd8b9453d1
BigLorD <FuryTr@yandex.com>
2019-03-01 19:15:10 +0200

    update

--- pvpgn-magic-builder-master/MySQL.conf.bat
+++ pvpgn-magic-builder-master/MySQL.conf.bat
@@ -1,7 +1,7 @@
 @echo off
 set DB_VERSION=5.7.21
-set _db_host=127.0.0.1
-set _db_user=pvpgn
-set _db_password=
+set _db_host=localhost
+set _db_user=root
+set _db_password=vertrigo
 set _db_name=pvpgn
 set _db_prefix=
